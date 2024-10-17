0c7eb17cd215b30919df022ea6d62cf6489ad92b
Oleksandr Verozub <w69953@student.wsiz.edu.pl>
2024-10-17 11:23:51 +0200

    first commit

---
+++ lab2_oop/Constructions/Construction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_oop.Constructions
+{
+    internal class Construction
+    {
+        public Construction()
+        {
+            
+        }
+        public Construction(float height, float width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
---
+++ lab2_oop/Program.cs
@@ -0,0 +1,7 @@
+﻿using lab2_oop.Constructions;
+using System.Security.Principal;
+
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Material");
+
+Console.WriteLine(construction2.Height);
